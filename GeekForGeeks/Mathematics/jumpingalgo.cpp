51f04284c93a3ce089371e8c6e9b55100fc5358e
Sayanto Roy <sroy.sayanto@gmail.com>
2020-05-18 21:20:56 +0530

    Commit

---
+++ GeekForGeeks/Mathematics/jumpingalgo.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+
+int gcd(int b , int a)
+{
+    if(b==0)
+        return a;
+    else
+    {
+        gcd(a%b , b);
+    }
+}
+
+
+
+int main()
+{
+    int n,k;
+    cin>>n;
+    cin>>k;
+    int a[n];
+    int D = gcd(n , k);
+
+    for(int i=0;i<n;i++)
+    {
+        int kk;
+        cin>>k;
+        a[i]=k;
+    }
+
+    for(int i=0;i<D;i++)
+    {
+        int temp = a[i];int l;
+        cout<<temp<<" = temp"<<endl;
+
+        for(int j=i;j<n;j+=D)
+        {
+            a[j] = a[j+D];
+            l=j;
+        }
+        a[l] = temp;
+    }
+
+
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+
+}
