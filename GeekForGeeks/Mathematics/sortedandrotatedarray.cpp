c6e4f7db7dbb49e691097f2cbbec4fdbe8c7228c
Sayanto Roy <sroy.sayanto@gmail.com>
2020-06-05 11:57:33 +0530

    Commit

--- GeekForGeeks/Mathematics/sortedandrotatedarray.cpp
+++ GeekForGeeks/Mathematics/sortedandrotatedarray.cpp
@@ -30,24 +30,6 @@ int pivot(int *a , int x ,int y)
 
 }
 
-/*int pivot(int arr[], int low, int high)
-{
-  // base cases
-  if (high < low) return -1;
-  if (high == low) return low;
-
-   int mid = (low + high)/2; /*low + (high - low)/2;
-   if (arr[mid] > arr[mid + 1])
-    return mid;
-
-   if (arr[mid] < arr[mid - 1])
-    return (mid-1);
-
-   if (arr[low] >= arr[mid])
-    return pivot(arr, low, mid-1);
-
-   return pivot(arr, mid + 1, high);
-}*/
 
 int main()
 {
