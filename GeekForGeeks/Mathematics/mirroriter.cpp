6f0e482e254e345e1e2a4608b2e8d192e9b598d5
Sayanto Roy <sroy.sayanto@gmail.com>
2020-07-26 01:33:13 +0530

    Commit

---
+++ GeekForGeeks/Mathematics/mirroriter.cpp
@@ -0,0 +1,85 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node{
+    int data;
+    node* left;
+    node* right;
+    node(int x)
+    {
+        this->data = x;
+        this->left = this->right = NULL;
+    }
+};
+
+
+
+//Recursive Way To Print It
+void mirror(node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    node* temp;
+    node *y = new node(-1);
+    while(!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+        if(temp->data!=-1 && temp)
+        {
+            cout<<temp->data;
+        }
+
+        if(temp==NULL)
+        {
+            cout<<"asd";
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else
+        {
+            if(temp->right)
+                q.push(temp->right);
+
+            if(temp->left)
+                q.push(temp->left);
+
+
+
+        }
+    }
+
+}
+
+void inorder(node* root)
+{
+    if(root)
+    {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+int main()
+{
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(100);
+    inorder(root);
+    cout<<"\nMirror of the tree : \n";
+    //Recursive
+    mirror(root);
+
+}
+
