5cfb3174041efefe5e8144ac2b634239ac8ddff1
Sayanto Roy <sroy.sayanto@gmail.com>
2020-07-27 00:12:23 +0530

    Commit

--- GeekForGeeks/Trees/maxinbt.cpp
+++ GeekForGeeks/Trees/maxinbt.cpp
@@ -14,34 +14,69 @@ struct node
     }
 };
 
-int maxintree(node* root)
-{
-    int rootVal , leftVal , rightVal,maxx = INT_MIN;
-    if(root)
-    {
-        rootVal = root->data;
-        leftVal = maxintree(root->left);
-        rightVal = maxintree(root->right);
 
-        maxx = max(max(leftVal, rightVal),rootVal);
+node* buildTree(int pre[], int in[] ,int preS,int preE ,int inS, int inE)
+{
+    if(inE < inS)
+    {
+        return NULL;
     }
-    return maxx;
+
+    int k = pre[preS];
+    int rootIndex = -1;
+    for(int i =inS;i<=inE ;i++)
+    {
+        if(in[i] == k)
+            rootIndex = i;
+    }
+
+    int inLS = inS;
+    int inLE = rootIndex -1;
+    int preLS = preS + 1;
+    int preLE = inLE - inLS + preLS;
+    int preRS = preLE + 1;
+    int preRE = preE;
+    int inRS = rootIndex+1;
+    int inRE = inE;
+    node* root = new node(k);
+    root->left = buildTree(pre, in , preLS, preLE , inLS, inLE);
+    root->right = buildTree(pre, in , preRS, preRE , inRS, inRE);
+    return root;
+
 }
 
+void inorder(node *root)
+{
+    if(root)
+    {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
 
 int main()
 {
-    node *root = new node(1);
-    root->left = new node(2);
-    root->right = new node(3);
-    root->right->left = new node(31);
-    root->left->left = new node(4);
-    root->left->right = new node(5);
-    root->right->left = new node(6);
-    root->right->right = new node(7);
+    node *root = NULL;
+    int n;
+    cin>>n;
+    int pre[n];
+    int in[n];
+    for(int i = 0; i<n;i++)
+    {
+        int k;
+        cin>>k;
+        pre[i] = k;
+    }
+    for(int i = 0; i<n;i++)
+    {
+        int k;
+        cin>>k;
+        in[i] = k;
+    }
 
-    int k = maxintree(root);
-    cout<<k;
-
+    root = buildTree(pre , in , 0 , n-1 , 0 , n-1);
+    cout<<"Tree Built : And Inorder Traversal \n";
+    inorder(root);
     return 0;
 }
--- GeekForGeeks/Trees/traversalstrees.cpp
+++ GeekForGeeks/Trees/traversalstrees.cpp
@@ -48,11 +48,13 @@ int main()
 {
     node *root = new node(1);
     root->left = new node(2);
+    root->left->left = new node(5);
+    root->left->left->left = new node(7);
+    root->left->right = new node(4);
     root->right = new node(3);
-    root->left->left = new node(4);
-    root->left->right = new node(5);
     root->right->left = new node(6);
-    root->right->right = new node(7);
+    root->right->left->right = new node(8);
+    root->right->left->right->left = new node(9);
     cout<<"PreOrder Traversal"<<endl;
     preorder(root);
     cout<<"\nPostOrder Traversal"<<endl;
