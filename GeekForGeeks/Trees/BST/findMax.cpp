ae06a81332c8969b1416c1a9bba7ba7f5acb30b2
Sayanto Roy <sroy.sayanto@gmail.com>
2020-07-30 23:43:53 +0530

    Commit

---
+++ GeekForGeeks/Trees/BST/findMax.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+  int data;
+  node* left;
+  node* right;
+};
+
+node* insertBST(node* root , int x)
+{
+
+    if(root==NULL)
+    {
+        node* root= new node;
+        root->data = x;
+        root->left = root->right = NULL;
+        return root;
+    }
+    else
+    {
+        if(x <root->data)
+        {
+            root->left = insertBST(root->left , x);
+        }
+        else
+        {
+            root->right = insertBST(root->right , x);
+        }
+
+    }
+}
+
+void findMax(node *root)
+{
+    if(!root)
+    {
+        cout<<"No Tree Present"<<endl;
+        return;
+    }
+    else
+    {
+        while(root->right)
+        {
+            root = root->right;
+        }
+
+        cout<<"The Maximum Element in this tree is : "<<root->data<<endl;
+        return;
+    }
+}
+
+void inorder(node* root)
+{
+    if(root)
+    {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    node *root = new node;
+    root = NULL;
+    for(int i = 0 ;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        root= insertBST(root , k);
+    }
+
+    inorder(root);
+    cout<<endl;
+    findMax(root);
+    return 0;
+}
