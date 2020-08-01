47a052601c8ae63b92e122f942b2483da51bd990
Sayanto Roy <sroy.sayanto@gmail.com>
2020-08-01 20:05:33 +0530

    Commit

---
+++ GeekForGeeks/Trees/BST/heightBST.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+  int data;
+  node* left;
+  node* right;
+};
+
+node* insertBST(node* root , int x)
+{
+
+    if(root==NULL)
+    {
+        node* root= new node;
+        root->data = x;
+        root->left = root->right = NULL;
+        return root;
+    }
+    else
+    {
+        if(x <root->data)
+        {
+            root->left = insertBST(root->left , x);
+        }
+        else
+        {
+            root->right = insertBST(root->right , x);
+        }
+
+    }
+}
+
+void inorder(node* root)
+{
+    if(root)
+    {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+
+int heightBST(node *root)
+{
+    if(!root)
+        return 0;
+
+    return 1+ max(heightBST(root->left),heightBST(root->right));
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    node *root = new node;
+    root = NULL;
+    for(int i = 0 ;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        root= insertBST(root , k);
+    }
+
+    inorder(root);
+    cout<<"\n The height of the BST : "<<heightBST(root);
+    return 0;
+}
