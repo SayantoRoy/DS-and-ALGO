fd0110705cbba014c4e4e1259016b296339d14f1
Sayanto Roy <sroy.sayanto@gmail.com>
2020-08-08 01:32:52 +0530

    Commit

--- GeekForGeeks/Trees/AVL Trees/buildAVL.cpp
+++ GeekForGeeks/Trees/AVL Trees/buildAVL.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -10,13 +9,22 @@ struct node
   node* right;
 };
 
+
+int height(node* l)
+{
+    if(!l)
+        return 0;
+    return l->height;
+}
+
+
 node* leftRotate(node* root)
 {
     node *temp = root->left;
     root->left = temp->right;
     temp->right = root;
-    root->height = max(root->left->height , root->right->height) +1;
-    temp->height = max(temp->left->height , root->height) + 1;
+    root->height = max(height(root->left) , height(root->right)) +1;
+    temp->height = max(height(temp->left) , root->height) + 1;
     return temp;
 }
 
@@ -25,8 +33,8 @@ node* rightRotate(node* root)
     node *temp = root->right;
     root->right = temp->left;
     temp->left = root;
-    root->height = max(root->left->height , root->right->height) +1;
-    temp->height = max(temp->right->height , root->height) + 1;
+    root->height = max(height(root->left) , height(root->right)) +1;
+    temp->height = max(height(temp->right), root->height) + 1;
     return temp;
 }
 
@@ -42,9 +50,13 @@ node* rightLeftRotate(node* root)
     return rightRotate(root);
 }
 
+
+
 int getDiff(node* left , node* right)
 {
-    int j = left->height - right->height;
+
+    int j = height(left) - height(right);
+
     return j;
 }
 
@@ -52,39 +64,56 @@ node* insertIn(node* root , int x)
 {
     if(!root)
     {
-        node* temp;
+        node* temp = new node;
         temp->data = x;
         temp->left = temp->right = NULL;
         temp->height = 1;
-
+        cout<<"sad"<<endl;
         return temp;
     }
-    else
-    {
-        if(root->data > x){
-                cout<<"Eir";
-                root->left = insertIn(root->left , x);
-            cout<<"asd";
-        }
 
+    if(root->data > x)
+        {
+            root->left = insertIn(root->left , x);
+        }
     if(root->data < x)
-        root->right = insertIn(root->right , x);
+        {
+            root->right = insertIn(root->right , x);
+        }
     else
         return root;
 
-    root->height = max(root->left->height , root->right->height) +1;
-
+    root->height = max(height(root->left) , height(root->right)) +1;
     int val = getDiff(root->left , root->right);
+    cout<<val<<" "<<root->right->data<<" "<<x<<endl;
+
 
     if(val>1 && root->left->data > x)
+    {
+        cout<<"Asd1";
         return leftRotate(root);
-    if(val<-1 && root->right->data < x)
+    }
+    if(val<-1 && root->left->data< x)
+    {
+        cout<<"Asd2";
         return leftRightRotate(root);
-    if(val >1 && root->right->data > x)
+    }
+    if(val>1 && root->right->data < x)
+    {
+        cout<<"Asd3";
         return rightLeftRotate(root);
-    if(val<-1 && root->right->data < x)
+    }
+    if((val<-1) && (root->right->data < x))
+    {
+        cout<<"Asd4";
         return rightRotate(root);
     }
+
+
+
+    return root;
+
+
 
 
     return root;
@@ -117,5 +146,3 @@ int main()
 
     return 0;
 }
-
-
