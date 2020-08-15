f7792d58e73684c9e674bdae0895dd34b40342cb
Sayanto Roy <sroy.sayanto@gmail.com>
2020-08-15 12:57:35 +0530

    Commit

---
+++ GeekForGeeks/Trees/Heaps & PQ/insertInHeap.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> heapify(vector<int> a , int i)
+{
+    int maxs;
+    int n = a.size();
+    int l = i*2 +1;
+    int r = i*2 +2;
+    if(l<n && a[l] > a[i])
+        maxs = l;
+    else
+        maxs = i;
+    if(r<n && a[r] > a[maxs])
+        maxs = r;
+
+    if(maxs!= i)
+    {
+        cout<<a[i]<< " "<<a[maxs]<<endl;
+        swap(a[i] , a[maxs]);
+        cout<<a[i]<< " "<<a[maxs]<<endl;
+        heapify(a , maxs);
+    }
+    return a;
+}
+
+
+vector<int> insert(vector<int> a , int x)
+{
+    a.push_back(x);
+    int n = a.size();
+    for(int i = (n/2 -1) ; i>=0;i--)
+    {
+        a = heapify(a , i);
+        cout<<a[i]<<" "<<endl;
+    }
+
+
+    return a;
+}
+
+void printHeap(vector<int> a)
+{
+    for(int i = 0;i<a.size();i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int n ;
+    cin>>n;
+
+    vector<int> a;
+    for(int i =0;i<n;i++)
+    {
+        int k;
+        cin>>k;
+        a.push_back(k);
+    }
+    printHeap(a);
+    int jj;
+    cin>>jj;
+    a = insert(a , jj);
+    printHeap(a);
+}
