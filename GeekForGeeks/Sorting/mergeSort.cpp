8053c40ac3b90202c31c1c379bd30032fe15823b
Sayanto Roy <sroy.sayanto@gmail.com>
2020-08-29 23:40:25 +0530

    Commit

--- GeekForGeeks/Sorting/mergeSort.cpp
+++ GeekForGeeks/Sorting/mergeSort.cpp
@@ -1,9 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
 void mergeIt(int *a, int *temp , int left , int mid , int right )
 {
+    int k; int i; int j;
+    k =0;
+    i =0 ; j = 0;
+    int nl = left - mid;
+    int nr = mid+1 - right;
+    while(i<nl && j<nr)
+    {
+        if(a[i] < a[j])
+        {
+            temp[k] = a[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            temp[k] = a[j];
+            j++;
+            k++;
+        }
+    }
 
+    while(i<nl)
+    {
+        temp[k] = a[i];
+        i++;
+        k++;
+    }
+    while(j<nr)
+    {
+        temp[k] = a[j];
+        j++;
+        k++;
+    }
+
+    return;
 }
 
 void mergeSort(int *a, int *temp, int left , int right)
@@ -29,7 +65,8 @@ int main()
         cin>>k;
         a[i] = k;
     }
-    int *temp = new int[n];
+
+
     mergeSort(a, temp, 0, n-1);
     for(int i =0 ;i<n;i++)
     {
