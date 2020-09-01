714b8b29c547da87de9f0834e37f11001c04cc11
Sayanto Roy <sroy.sayanto@gmail.com>
2020-09-02 00:11:04 +0530

    Commit

--- GeekForGeeks/Sorting/heapSort.cpp
+++ GeekForGeeks/Sorting/heapSort.cpp
@@ -1,1 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void heapify(int *a , int n , int i)
+{
+    int maxs;
+    int l = i*2+1;
+    int r = i*2+2;
+
+    if(l<n && a[l]>a[i])
+    {
+        maxs = l;
+    }
+    else
+    {
+        maxs = i;
+    }
+    if(r<n&& a[r]>a[maxs])
+    {
+        maxs = r;
+    }
+
+    if(maxs != i)
+    {
+        swap(a[maxs] , a[i]);
+        heapify(a , n , maxs);
+    }
+}
+
+void heapSort(int *a , int n)
+{
+
+    for(int i = (n/2)-1 ; i>=0;i--)
+    {
+        heapify(a , n , i);
+    }
+
+    for(int i = n-1;i>=0;i--)
+    {
+        swap(a[0] , a[i]);
+        heapify(a ,i , 0);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *a = new int[n];
+    for(int i = 0; i<n;i++)
+    {
+        int k;
+        cin>>k;
+        a[i] = k;
+    }
+    heapSort(a , n);
+    for(int i = 0 ;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
 
