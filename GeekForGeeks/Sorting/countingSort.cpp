9453eaa039311e97019c50bd2797af73bf99ae3c
Sayanto Roy <sroy.sayanto@gmail.com>
2020-09-03 23:56:03 +0530

    Commit

--- GeekForGeeks/Sorting/countingSort.cpp
+++ GeekForGeeks/Sorting/countingSort.cpp
@@ -3,45 +3,55 @@ using namespace std;
 
 void countSort(int *a , int n)
 {
-    int max = -100;
-    for(int i =0 ;i<n;i++)
+    int b[n];
+    int max = -1;
+    for(int i =0;i<n;i++)
     {
-        if(max < a[i])
+        if(a[i]>max)
         {
             max = a[i];
         }
     }
-    int b[n];
-    int c[max];
-    for(int i =0 ; i<max;i++)
+
+    int c[max+1];
+    for(int i =0 ;i<max+1;i++)
     {
         c[i] = 0;
     }
 
-    for(int i =0 ; i<n;i++)
+
+    for(int i = 0; i<n;i++)
     {
         c[a[i]] = c[a[i]]+1;
     }
 
-    for(int i = 1; i<n;i++)
+    for(int i = 1 ; i<=max ; i++)
     {
         c[i] = c[i]+c[i-1];
     }
 
-    for(int i =0 ; i<n;i++)
+
+    for(int i = n-1 ;i>=0;i--)
     {
-        cout<<c[i]<<" ";
+        b[c[a[i]]] = a[i];
+        c[a[i]] = c[a[i]] -1;
+    }
+
+
+    for(int i = 0 ;i <n;i++)
+    {
+        cout<<b[i]<< " ";
     }
 }
 
 
-void printArray(int *a , int n)
+/*void printArray(int *a , int n)
 {
     for(int i =0; i<n;i++)
     {
         cout<<a[i]<<" ";
     }
-}
+}*/
 
 int main()
 {
@@ -56,6 +66,6 @@ int main()
     }
 
     countSort(a , n);
-    printArray(a , n);
+    //printArray(a , n);
     return 0;
 }
